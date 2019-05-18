87d204cba2d096d877a54e88a3510f76f086fdd0
Joe <tian2@oakland.edu>
2019-05-18 18:01:36 -0400

    change Int16

--- gem_speed_control/src/GemSpeedControl.cpp
+++ gem_speed_control/src/GemSpeedControl.cpp
@@ -6,8 +6,8 @@ GemSpeedControl::GemSpeedControl(ros::NodeHandle n, ros::NodeHandle pn)
 {
   sub_cmd = n.subscribe("cmd_vel", 1, &GemSpeedControl::recvCmd, this);
   sub_feedback = n.subscribe("c_speed", 1, &GemSpeedControl::recvFeedback, this);
-  pub_brake = n.advertise<std_msgs::Float64>("brake_cmd", 1);
-  pub_throttle = n.advertise<std_msgs::Float64>("throttle_cmd", 1);
+  pub_brake = n.advertise<std_msgs::Int16>("brake_cmd", 1);
+  pub_throttle = n.advertise<std_msgs::Int16>("throttle_cmd", 1);
   
   cmd_speed = 0.0;
   error_int = 0.0;
@@ -42,13 +42,15 @@ void GemSpeedControl::recvFeedback(const std_msgs::Float32ConstPtr& msg)
     throttle_pct = 1.0;
   }
   
-  std_msgs::Float64 throttle_cmd;
-  throttle_cmd.data = throttle_pct;
-  pub_throttle.publish(throttle_cmd);
+  std_msgs::Int16 acc;
+//  acc.data = (int)(180+32*throttle_pct);
+  acc.data = (int)(180+32*cmd_speed);
+  pub_throttle.publish(acc);
   
-  std_msgs::Float64 brake_cmd;
-  brake_cmd.data = brakes ? 100.0 : 0.0;
-  pub_brake.publish(brake_cmd);
+  std_msgs::Int16 brake;
+  brake.data = 0;
+  if(brakes) brake.data = 100;
+  pub_brake.publish(brake);
 }
 
 }
