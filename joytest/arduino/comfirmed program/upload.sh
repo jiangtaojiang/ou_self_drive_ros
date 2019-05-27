cf35ddde24a5ebeae56b43bc4f4d040b02a4fe64
Joe <tian2@oakland.edu>
2019-05-27 16:18:05 -0400

    update speed measurement & line camera

--- joytest/arduino/comfirmed program/upload.sh
+++ joytest/arduino/comfirmed program/upload.sh
@@ -1,8 +1,8 @@
 cd accelerate_control7
 arduino --board arduino:avr:mega:cpu=atmega2560 --port /dev/gem2/accbrake --upload accelerate_control7.ino 
 
-cd ../speed_measure7
-arduino --board arduino:avr:uno --port /dev/gem2/speedsens --upload speed_measure7.ino
+cd ../speed_measure8
+arduino --board arduino:avr:uno --port /dev/gem2/speedsens --upload speed_measure8.ino
 
 cd ../Steering_motor_with_Estop7
 arduino --board arduino:avr:uno --port /dev/gem2/steering --upload Steering_motor_with_Estop7.ino 
